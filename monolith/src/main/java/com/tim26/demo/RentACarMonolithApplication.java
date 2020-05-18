623ad00c0905966d78b650ce2a37675a022dba72
Ognjen <ognjengari@gmail.com>
2020-05-18 21:46:48 +0200

    initial commit

---
+++ monolith/src/main/java/com/tim26/demo/RentACarMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.tim26.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarMonolithApplication.class, args);
+	}
+
+}
---
+++ monolith/src/test/java/com/tim26/demo/RentACarMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim26.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
