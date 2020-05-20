bb2ac4877be86adf11bbd43a5be98b3bdfd54aa3
tjncc <55355686+tjncc@users.noreply.github.com>
2020-05-21 01:10:26 +0200

    updated end user model

--- monolith/src/main/java/com/tim26/demo/model/EndUser.java
+++ monolith/src/main/java/com/tim26/demo/model/EndUser.java
@@ -4,4 +4,30 @@ import javax.persistence.Entity;
 
 @Entity
 public class EndUser extends User {
+
+    private String firstname;
+    private String lastname;
+
+    public EndUser() {
+    }
+
+    public EndUser(Role role, String email, String password, String username, String firstname, String lastname) {
+        super(role, email, password, username);
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
