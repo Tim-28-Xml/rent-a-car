00c8a093e59af985e6342a33173a76af510a9f8e
Mina Maras <minamaras1997@gmail.com>
2020-05-21 17:12:28 +0200

    Microservice architecture

---
+++ ChatService/src/test/java/com/tim26/ChatService/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim26.ChatService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
