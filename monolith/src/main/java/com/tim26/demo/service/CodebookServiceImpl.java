cf284ab380da3aef2aa1da61029577b680f3bace
Mina Maras <minamaras1997@gmail.com>
2020-05-31 19:19:31 +0200

    Update CodebookServiceImpl.java

--- monolith/src/main/java/com/tim26/demo/service/CodebookServiceImpl.java
+++ monolith/src/main/java/com/tim26/demo/service/CodebookServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tim26.demo.service;
 
+import com.tim26.demo.model.BrandModels;
 import com.tim26.demo.model.Codebook;
 import com.tim26.demo.repository.CodebookRepository;
 import com.tim26.demo.service.interfaces.CodebookService;
@@ -25,28 +26,40 @@ public class CodebookServiceImpl implements CodebookService {
         return codebookRepository.save(codebook);
     }
 
-    /*@Override
-    public List<String> getFuelTypes() {
-        return codebookRepository.findAllByFuelTypes();
+    @Override
+    public List<BrandModels> getBrands() {
+       Codebook codebook = getFirstCodebook();
+       return codebook.getBrandModels();
     }
 
     @Override
-    public List<String> getTransmissionTypes() {
-        return codebookRepository.findAllByTransmissionTypes();
-    }
-
-    @Override
-    public List<String> getBrands() {
-        return codebookRepository.findAllByBrandModels();
-    }
-
-    @Override
-    public List<String> getModels() {
+    public List<String> getModelsFromBrand(String brand) {
+        BrandModels brandmodels = new BrandModels();
+        for(BrandModels b : getBrands()) {
+            if(b.getBrand().equals(brand)) {
+                return b.getModels();
+            }
+        }
         return null;
     }
 
     @Override
+    public List<String> getFuelTypes() {
+        Codebook codebook = getFirstCodebook();
+        return codebook.getFuelTypes();
+    }
+
+    @Override
+    public List<String> getTransmissionTypes() {
+        Codebook codebook = getFirstCodebook();
+        return codebook.getTransmissionTypes();
+    }
+
+    @Override
     public List<String> getCarClasses() {
-        return codebookRepository.findAllByCarClasses();
-    }*/
+        Codebook codebook = getFirstCodebook();
+        return codebook.getCarClasses();
+    }
+
+
 }
