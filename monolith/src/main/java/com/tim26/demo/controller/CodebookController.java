b27866ff5b480d2029a57517c47bb03d139320aa
Mina Maras <minamaras1997@gmail.com>
2020-05-31 19:18:46 +0200

    codebook controller

--- monolith/src/main/java/com/tim26/demo/controller/CodebookController.java
+++ monolith/src/main/java/com/tim26/demo/controller/CodebookController.java
@@ -1,19 +1,19 @@
 package com.tim26.demo.controller;
 
 
+import com.tim26.demo.model.BrandModels;
 import com.tim26.demo.model.Codebook;
 import com.tim26.demo.service.interfaces.CodebookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/codebook")
 public class CodebookController {
@@ -28,21 +28,33 @@ public class CodebookController {
         return new ResponseEntity<>(codebookService.getFirstCodebook(), HttpStatus.OK);
     }
 
-    /*@GetMapping(value = "/fuel-types")
-    @PreAuthorize("hasRole('CREATE_AD')")
+    @PreAuthorize("hasAuthority('CREATE_AD')")
+    @GetMapping(value = "/fuel-types")
     public ResponseEntity<List<String>> getFuelTypes(){
         return new ResponseEntity<List<String>>(codebookService.getFuelTypes(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/transmission-types")
-    @PreAuthorize("hasRole('CREATE_AD')")
+    @PreAuthorize("hasAuthority('CREATE_AD')")
     public ResponseEntity<List<String>> getTransmissionTypes(){
         return new ResponseEntity<List<String>>(codebookService.getTransmissionTypes(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/car-classes")
-    @PreAuthorize("hasRole('CREATE_AD')")
+    @PreAuthorize("hasAuthority('CREATE_AD')")
     public ResponseEntity<List<String>> getCarClasses(){
         return new ResponseEntity<List<String>>(codebookService.getCarClasses(), HttpStatus.OK);
-    }*/
+    }
+
+    @GetMapping(value = "/brands")
+    @PreAuthorize("hasAuthority('CREATE_AD')")
+    public ResponseEntity<List<BrandModels>> getBrands(){
+        return new ResponseEntity<List<BrandModels>>(codebookService.getBrands(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/models-from-brand/{brand}")
+    @PreAuthorize("hasAuthority('CREATE_AD')")
+    public ResponseEntity<List<String>> getModels(@PathVariable String brand){
+        return new ResponseEntity<List<String>>(codebookService.getModelsFromBrand(brand), HttpStatus.OK);
+    }
 }
