c07d51e019ca582b216c06b229bd2dd28cef13ea
Mina Maras <minamaras1997@gmail.com>
2020-05-31 19:19:35 +0200

    Update AdService.java

--- monolith/src/main/java/com/tim26/demo/service/interfaces/AdService.java
+++ monolith/src/main/java/com/tim26/demo/service/interfaces/AdService.java
@@ -1,4 +1,12 @@
 package com.tim26.demo.service.interfaces;
 
+import com.tim26.demo.dto.CreateAdDto;
+import com.tim26.demo.model.Ad;
+
+import java.security.Principal;
+import java.util.List;
+
 public interface AdService {
+    boolean save(CreateAdDto ad, Principal p);
+    List<Ad> getAllAds();
 }
