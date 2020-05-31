305e861446ea1d8ad0bcd7b4b61e1f827ad660fa
Mina Maras <minamaras1997@gmail.com>
2020-05-31 19:18:51 +0200

    Update CreateAdDto.java

--- monolith/src/main/java/com/tim26/demo/dto/CreateAdDto.java
+++ monolith/src/main/java/com/tim26/demo/dto/CreateAdDto.java
@@ -1,4 +1,111 @@
 package com.tim26.demo.dto;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateAdDto {
+    private String brand;
+    private String model;
+    private String fuel;
+    private String transmission;
+    private String carClass;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double km;
+    private double kmLimit;
+    private boolean collision;
+    private String childSeats;
+
+    public CreateAdDto() {
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getCarClass() {
+        return carClass;
+    }
+
+    public void setCarClass(String carClass) {
+        this.carClass = carClass;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public double getKmLimit() {
+        return kmLimit;
+    }
+
+    public void setKmLimit(double kmLimit) {
+        this.kmLimit = kmLimit;
+    }
+
+    public String getChildSeats() {
+        return childSeats;
+    }
+
+    public void setChildSeats(String childSeats) {
+        this.childSeats = childSeats;
+    }
+
+    public boolean isCollision() {
+        return collision;
+    }
+
+    public void setCollision(boolean collision) {
+        this.collision = collision;
+    }
 }
