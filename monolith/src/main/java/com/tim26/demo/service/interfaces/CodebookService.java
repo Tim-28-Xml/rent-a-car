be510f8b36b0be87fbdb8fd05b472586c73b77bd
Mina Maras <minamaras1997@gmail.com>
2020-05-31 19:19:37 +0200

    Update CodebookService.java

--- monolith/src/main/java/com/tim26/demo/service/interfaces/CodebookService.java
+++ monolith/src/main/java/com/tim26/demo/service/interfaces/CodebookService.java
@@ -1,5 +1,6 @@
 package com.tim26.demo.service.interfaces;
 
+import com.tim26.demo.model.BrandModels;
 import com.tim26.demo.model.Codebook;
 
 import java.util.ArrayList;
@@ -11,14 +12,13 @@ public interface CodebookService {
 
     Codebook save(Codebook codebook);
 
-    /*List<String> getFuelTypes();
+    List<BrandModels> getBrands();
+
+    List<String> getModelsFromBrand(String brand);
+
+    List<String> getFuelTypes();
 
     List<String> getTransmissionTypes();
 
-    List<String> getBrands();
-
-    List<String> getModels();
-
-    List<String> getCarClasses();*/
-
+    List<String> getCarClasses();
 }
