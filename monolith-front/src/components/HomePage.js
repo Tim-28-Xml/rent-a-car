7993d622c2c53bd37bf6fd82bfbcbbe8d9252e85
Mina Maras <minamaras1997@gmail.com>
2020-06-02 21:11:50 +0200

    Fix homepage layout

--- monolith-front/src/components/HomePage.js
+++ monolith-front/src/components/HomePage.js
@@ -121,7 +121,7 @@ class HomePage extends React.Component {
                 <div>
                     <h1 style={{ color: 'rgb(110,120,130)', textAlign: 'center', margin: "2% 0 0 0" }}>Welcome to Rent a Car</h1>
                     <CreateNewAd role={this.state.roles[0]}></CreateNewAd>
-                    <h1 style={{ color: 'rgb(110,120,130)', textAlign: 'center', margin: "2% 0 0 0" }}>Welcome to Rent a Car</h1>
+                    <h1 style={{ color: 'rgb(110,120,130)', textAlign: 'center', margin: "2% 0 0 0" }}>Ads</h1>
                     <div className="renderCardsAds">
                         {this.renderAdCards()}
                     </div>
