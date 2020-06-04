f73f684bd55da048bfc4e43bd84336c7ea177c2d
Ognjen <ognjengari@gmail.com>
2020-06-04 16:51:59 +0200

    preauth excluded adservice

--- AdService/src/main/java/com/tim26/AdService/controller/TransmissionTypeController.java
+++ AdService/src/main/java/com/tim26/AdService/controller/TransmissionTypeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transmissionType")
-@PreAuthorize("hasRole('ADMIN')")
+//@PreAuthorize("hasRole('ADMIN')")
 public class TransmissionTypeController {
     @Autowired
     CodebookService codebookService;
