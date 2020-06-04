6ef3b3385c6fae38a0864c6a50741659c025cf0a
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-04 18:01:21 +0200

    added car repository

---
+++ AdService/src/main/java/com/tim26/AdService/repository/CarRepository.java
@@ -0,0 +1,12 @@
+package com.tim26.AdService.repository;
+
+import com.tim26.AdService.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+    Optional<Car> findById(Long id);
+
+}
