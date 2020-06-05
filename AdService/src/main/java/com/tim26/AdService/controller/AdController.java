8088fe71fda35466a3ae6079c32a926601ab9099
Mina Maras <minamaras1997@gmail.com>
2020-06-05 09:07:26 +0200

    implemented save method in controller

--- AdService/src/main/java/com/tim26/AdService/controller/AdController.java
+++ AdService/src/main/java/com/tim26/AdService/controller/AdController.java
@@ -2,16 +2,15 @@ package com.tim26.AdService.controller;
 
 import com.tim26.AdService.dto.AdDTO;
 import com.tim26.AdService.dto.CarDTO;
+import com.tim26.AdService.dto.CreateAdDto;
 import com.tim26.AdService.service.interfaces.AdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -49,6 +48,14 @@ public class AdController {
         CarDTO dto = adService.findCarById(Long.parseLong(id));
         return new ResponseEntity<>(dto,HttpStatus.OK);
 
+    }
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<CreateAdDto> save(@RequestBody CreateAdDto createAdDto) {
+        if(adService.save(createAdDto))
+            return new ResponseEntity<>(createAdDto, HttpStatus.OK);
+
+        return new ResponseEntity<>(createAdDto, HttpStatus.BAD_REQUEST);
     }
 
 }
