34f882084d97f987a5e8cf157f38715f504669df
Mina Maras <minamaras1997@gmail.com>
2020-06-05 09:06:49 +0200

    sql file

---
+++ AdService/src/main/java/com/tim26/AdService/dto/CreateAdDto.java
@@ -0,0 +1,153 @@
+package com.tim26.AdService.dto;
+
+import com.tim26.AdService.model.DateRange;
+
+import java.util.List;
+
+public class CreateAdDto {
+    private String brand;
+    private String model;
+    private String fuel;
+    private String transmission;
+    private String carClass;
+    private List<DateRange> dates;
+    private double km;
+    private double kmLimit;
+    private boolean collision;
+    private String childSeats;
+    private List<String> files;
+    private String city;
+    private String role;
+    private Long userId;
+
+    public CreateAdDto() {
+
+    }
+
+    public CreateAdDto(String brand, String model, String fuel, String transmission, String carClass, List<DateRange> dates, double km, double kmLimit, boolean collision, String childSeats, List<String> files, String city) {
+        this.brand = brand;
+        this.model = model;
+        this.fuel = fuel;
+        this.transmission = transmission;
+        this.carClass = carClass;
+        this.dates = dates;
+        this.km = km;
+        this.kmLimit = kmLimit;
+        this.collision = collision;
+        this.childSeats = childSeats;
+        this.files = files;
+        this.city = city;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getCarClass() {
+        return carClass;
+    }
+
+    public void setCarClass(String carClass) {
+        this.carClass = carClass;
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public double getKmLimit() {
+        return kmLimit;
+    }
+
+    public void setKmLimit(double kmLimit) {
+        this.kmLimit = kmLimit;
+    }
+
+    public String getChildSeats() {
+        return childSeats;
+    }
+
+    public void setChildSeats(String childSeats) {
+        this.childSeats = childSeats;
+    }
+
+    public boolean isCollision() {
+        return collision;
+    }
+
+    public void setCollision(boolean collision) {
+        this.collision = collision;
+    }
+
+    public List<String> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<String> files) {
+        this.files = files;
+    }
+
+    public List<DateRange> getDates() {
+        return dates;
+    }
+
+    public void setDates(List<DateRange> dates) {
+        this.dates = dates;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
