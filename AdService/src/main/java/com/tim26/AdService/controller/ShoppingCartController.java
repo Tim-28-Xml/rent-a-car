511d8e09a02a1b90e9c832313bb43fd1854e0cd5
tjncc <55355686+tjncc@users.noreply.github.com>
2020-06-14 22:42:00 +0200

    changed request mapping

--- AdService/src/main/java/com/tim26/AdService/controller/ShoppingCartController.java
+++ AdService/src/main/java/com/tim26/AdService/controller/ShoppingCartController.java
@@ -13,7 +13,7 @@ import java.security.Principal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shoppingCart")
+@RequestMapping("/shoppingcart")
 @PreAuthorize("hasAuthority('ORDER')")
 public class ShoppingCartController {
 
