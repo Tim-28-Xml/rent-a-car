c6060b6581562a25be01bec21d513a67d6bece30
tjncc <55355686+tjncc@users.noreply.github.com>
2020-06-15 17:49:56 +0200

    changed response

--- AdService/src/main/java/com/tim26/AdService/controller/ShoppingCartController.java
+++ AdService/src/main/java/com/tim26/AdService/controller/ShoppingCartController.java
@@ -6,6 +6,7 @@ import com.tim26.AdService.model.User;
 import com.tim26.AdService.service.interfaces.ShoppingCartService;
 import com.tim26.AdService.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -34,11 +35,11 @@ public class ShoppingCartController {
     }
 
     @PostMapping
-    public ResponseEntity addToShoppingCart(@RequestBody AdCartDTO adCartDTO, Principal principal){
+    public ResponseEntity<?> addToShoppingCart(@RequestBody AdCartDTO adCartDTO, Principal principal){
         if(shoppingCartService.addAd(principal.getName(), Long.parseLong(adCartDTO.adId)))
             return ResponseEntity.ok().build();
         else
-            return ResponseEntity.badRequest().build();
+            return new ResponseEntity<>("You have already ordered this car!", HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping
