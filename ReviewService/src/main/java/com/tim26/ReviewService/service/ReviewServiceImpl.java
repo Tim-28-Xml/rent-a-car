c2d6ea98e0572d735a74a099e775bef21ddb5300
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-17 21:04:45 +0200

    created implementation of review service

---
+++ ReviewService/src/main/java/com/tim26/ReviewService/service/ReviewServiceImpl.java
@@ -0,0 +1,44 @@
+package com.tim26.ReviewService.service;
+
+import com.tim26.ReviewService.dto.ReviewDTO;
+import com.tim26.ReviewService.model.Ad;
+import com.tim26.ReviewService.model.Review;
+import com.tim26.ReviewService.repository.AdRepository;
+import com.tim26.ReviewService.repository.ReviewRepository;
+import com.tim26.ReviewService.service.interfaces.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private AdRepository adRepository;
+
+
+    @Override
+    public List<ReviewDTO> getAllByAd(Long id) {
+
+        List<ReviewDTO> reviews = new ArrayList<>();
+        Ad ad = adRepository.findById(id).get();
+
+       for(Review r: reviewRepository.findAllByAd(ad)){
+           if(r.isApproved()) {
+               ReviewDTO reviewDTO = new ReviewDTO(r);
+               reviews.add(reviewDTO);
+           }
+
+       }
+
+       return  reviews;
+
+    }
+
+
+}
