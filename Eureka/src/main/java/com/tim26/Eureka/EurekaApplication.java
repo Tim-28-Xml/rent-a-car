4df7ad3d48ac1b81c9639b63ac5b0f887a857fac
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-17 13:49:59 +0200

    added system property for ssl

--- Eureka/src/main/java/com/tim26/Eureka/EurekaApplication.java
+++ Eureka/src/main/java/com/tim26/Eureka/EurekaApplication.java
@@ -4,13 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.Properties;
+
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+		Properties props = System.getProperties();
+		props.setProperty("javax.net.debug","ssl,handshake");
 	}
 
 }
