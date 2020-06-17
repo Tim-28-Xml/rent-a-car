87acebb6fdbcdb081fece23411d8ad14008e5800
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-17 21:04:57 +0200

    added data in sql file

---
+++ ReviewService/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+--Users--
+insert into user (username) values ('agent1');
+insert into user( username) values ('user1');
+
+--Ads--
+
+insert into ad(id) values (-5);
+insert into ad(id) values (-2);
+
+--Reviews--
+insert into review(id,title,content,rating,time,ad_id,creator_username,approved) values ( -4,'Odlicno',
+'Kola su super a pregovori sa firmom su odlicno prosli,moja preporuka!',1,null,-5,'agent1',true);
+
+
+
+
+
+
