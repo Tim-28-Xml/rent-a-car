816ff3183f14017bcb66a204fc85ba692c377f1a
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-17 21:01:28 +0200

    added path for filtering

--- AuthenticationService/src/main/java/com/tim26/AuthenticationService/config/WebSecurityConfig.java
+++ AuthenticationService/src/main/java/com/tim26/AuthenticationService/config/WebSecurityConfig.java
@@ -76,6 +76,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/ads/all").permitAll()
                 .antMatchers("/api/ads/one/{id}").permitAll()
                 .antMatchers("/api/ads/car/{id}").permitAll()
+                .antMatchers("/api/ads/filter").permitAll()
 
                 // svaki zahtev mora biti autorizovan
                 .anyRequest().authenticated().and()
@@ -100,6 +101,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 "/**/*.css", "/**/*.js");
         web.ignoring().antMatchers(HttpMethod.POST,"/api/auth/login");
         web.ignoring().antMatchers(HttpMethod.GET,"/api/ads/all");
+        web.ignoring().antMatchers(HttpMethod.GET,"/api/ads/filter");
         web.ignoring().antMatchers(HttpMethod.GET,"/api/ads/one/{id}");
         web.ignoring().antMatchers(HttpMethod.GET,"/api/ads/car/{id}");
 
