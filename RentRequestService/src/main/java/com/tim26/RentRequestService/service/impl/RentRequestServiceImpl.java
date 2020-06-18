a5e6fc673145ccabe860f8b01c9c1a1a466de25a
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-18 19:25:34 +0200

    added method for getting all paid rent requests from logged in user

--- RentRequestService/src/main/java/com/tim26/RentRequestService/service/impl/RentRequestServiceImpl.java
+++ RentRequestService/src/main/java/com/tim26/RentRequestService/service/impl/RentRequestServiceImpl.java
@@ -1,16 +1,19 @@
 package com.tim26.RentRequestService.service.impl;
 
 import com.tim26.RentRequestService.controller.RentRequestController;
+import com.tim26.RentRequestService.dto.RentRequestDTO;
 import com.tim26.RentRequestService.dto.ViewRequestDTO;
 import com.tim26.RentRequestService.model.RentRequest;
 import com.tim26.RentRequestService.model.RequestStatus;
 import com.tim26.RentRequestService.model.User;
 import com.tim26.RentRequestService.repository.RentRequestRepository;
+import com.tim26.RentRequestService.repository.UserRepository;
 import com.tim26.RentRequestService.service.RentRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +21,9 @@ public class RentRequestServiceImpl implements RentRequestService {
 
     @Autowired
     RentRequestRepository rentRequestRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public RentRequest findById(Long id) {
@@ -52,6 +58,21 @@ public class RentRequestServiceImpl implements RentRequestService {
             return false;
         }
 
+    }
+
+    @Override
+    public List<RentRequestDTO> getPaidRequestFromUser(Principal p) {
+        List<RentRequest> requests = rentRequestRepository.findByCreator(userRepository.findById(p.getName()).get());
+        List<RentRequestDTO> dtos =  new ArrayList<>();
+
+        for(RentRequest r: requests){
+            if(r.getRequestStatus().equals(RequestStatus.PAID)) {
+                RentRequestDTO dto = new RentRequestDTO(r);
+                dtos.add(dto);
+            }
+        }
+
+        return dtos;
     }
 
 
