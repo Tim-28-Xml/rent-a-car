cbe4ddd46e1f617ba79570e2e85964f40db38ae1
Mina Maras <minamaras1997@gmail.com>
2020-06-18 19:22:21 +0200

    Update PricelistRepository.java

--- monolith/src/main/java/com/tim26/demo/repository/PricelistRepository.java
+++ monolith/src/main/java/com/tim26/demo/repository/PricelistRepository.java
@@ -1,9 +1,10 @@
 package com.tim26.demo.repository;
 
 import com.tim26.demo.model.PriceList;
+import com.tim26.demo.service.interfaces.PricelistService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PricelistRepository extends JpaRepository<PriceList, Long> {
     PriceList save(PriceList priceList);
-    boolean findByName(String name);
+    PriceList findByName(String name);
 }
