cd44dcf6571cf229020ab2619cd6b2d6e9d50060
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-19 15:06:30 +0200

    added method for finding all reviews by logged in user

--- ReviewService/src/main/java/com/tim26/ReviewService/repository/ReviewRepository.java
+++ ReviewService/src/main/java/com/tim26/ReviewService/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.tim26.ReviewService.repository;
 
 import com.tim26.ReviewService.model.Ad;
 import com.tim26.ReviewService.model.Review;
+import com.tim26.ReviewService.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     List<Review> findAllByAd(Ad ad);
+    List<Review> findAllByCreator(User user);
+
 }
