8ff067786b24215cedf959162b9b79c2c0520adc
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-19 15:07:28 +0200

    changed rating on existing reviews

--- ReviewService/src/main/resources/data.sql
+++ ReviewService/src/main/resources/data.sql
@@ -11,7 +11,7 @@ insert into ad(id) values (-2);
 
 --Reviews--
 insert into review(id,title,content,rating,time,ad_id,creator_username,approved) values ( -4,'Odlicno',
-'Kola su super a pregovori sa firmom su odlicno prosli,moja preporuka!',10,'2020-03-06 20:41:05.0+00
+'Kola su super a pregovori sa firmom su odlicno prosli,moja preporuka!',5,'2020-03-06 20:41:05.0+00
 ',-5,'user1',true);
 
 insert into review(id,title,content,rating,time,ad_id,creator_username,approved) values ( -5,'Katastrofa',
@@ -20,7 +20,7 @@ insert into review(id,title,content,rating,time,ad_id,creator_username,approved)
 
 
 insert into review(id,title,content,rating,time,ad_id,creator_username,approved) values ( -6,'Uredu',
-'Sve je bilo okej.',7,'2020-05-02 12:00:05.0+00
+'Sve je bilo okej.',3,'2020-05-02 12:00:05.0+00
 ',-5,'user5',false);
 
 
