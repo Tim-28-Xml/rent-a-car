4b3f8934c930c06c4213b5505876156637e7efdf
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-19 15:07:18 +0200

    added implementation for submiting review method

--- ReviewService/src/main/java/com/tim26/ReviewService/service/ReviewServiceImpl.java
+++ ReviewService/src/main/java/com/tim26/ReviewService/service/ReviewServiceImpl.java
@@ -1,14 +1,17 @@
 package com.tim26.ReviewService.service;
 
+import com.sun.org.apache.regexp.internal.RE;
 import com.tim26.ReviewService.dto.ReviewDTO;
 import com.tim26.ReviewService.model.Ad;
 import com.tim26.ReviewService.model.Review;
+import com.tim26.ReviewService.model.User;
 import com.tim26.ReviewService.repository.AdRepository;
 import com.tim26.ReviewService.repository.ReviewRepository;
 import com.tim26.ReviewService.service.interfaces.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -98,6 +101,40 @@ public class ReviewServiceImpl implements ReviewService {
         }
 
         return reviews;
+    }
+
+    @Override
+    public boolean submitReview(Principal p, ReviewDTO reviewDTO) {
+
+        Ad ad = new Ad();
+        ad.setId(reviewDTO.getAd_id());
+
+        User u = new User();
+        u.setUsername(p.getName());
+
+
+        Review review = new Review(reviewDTO,ad,u);
+
+        ad.getReviews().add(review);
+        u.getReviews().add(review);
+
+        reviewRepository.save(review);
+
+        return true;
+    }
+
+    @Override
+    public List<Long> getUserReviews(Principal p) {
+
+        List<Review> reviews = reviewRepository.findAllByCreator(new User(p.getName()));
+        List<Long> list_ids = new ArrayList<>();
+
+        for(Review r :  reviews){
+
+            list_ids.add(r.getAd().getId());
+
+        }
+        return list_ids;
     }
 
 
