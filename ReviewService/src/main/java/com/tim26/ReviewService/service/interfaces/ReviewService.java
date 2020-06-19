d91fa1bf45165de2ca124644be87df8ba9ba5db9
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-06-19 15:06:48 +0200

    added methods for subbmiting and getting all reviews from logged in user

--- ReviewService/src/main/java/com/tim26/ReviewService/service/interfaces/ReviewService.java
+++ ReviewService/src/main/java/com/tim26/ReviewService/service/interfaces/ReviewService.java
@@ -3,6 +3,7 @@ package com.tim26.ReviewService.service.interfaces;
 import com.tim26.ReviewService.dto.ReviewDTO;
 import com.tim26.ReviewService.model.Ad;
 
+import java.security.Principal;
 import java.util.List;
 
 public interface ReviewService {
@@ -12,5 +13,7 @@ public interface ReviewService {
     public boolean approvetReview(ReviewDTO reviewDTO);
     public boolean declineReview(Long id);
     public List<ReviewDTO> findAllUnapproved();
+    public boolean submitReview(Principal p,ReviewDTO  reviewDTO);
+    public List<Long> getUserReviews(Principal p);
 
 }
