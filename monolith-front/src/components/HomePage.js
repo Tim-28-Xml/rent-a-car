0d748c942542865da875cd85c62187b5e6be66a4
Mina Maras <minamaras1997@gmail.com>
2020-06-20 18:49:27 +0200

    Update HomePage.js

--- monolith-front/src/components/HomePage.js
+++ monolith-front/src/components/HomePage.js
@@ -122,7 +122,6 @@ class HomePage extends React.Component {
             return (
                 <div>
                     <h1 style={{ color: 'rgb(110,120,130)', textAlign: 'center', margin: "2% 0 0 0" }}>Welcome to Rent a Car</h1>
-                    <h1 style={{ color: 'rgb(110,120,130)', textAlign: 'center', margin: "2% 0 0 0" }}>Ads</h1>
                     <div className="renderCardsAds">
                         {this.renderAdCards()}
                     </div>
@@ -183,6 +182,7 @@ class HomePage extends React.Component {
                             {this.renderAdCards()}
                         </div>
                     </div>
+                    </div>
             )
         }
         else {
@@ -195,7 +195,7 @@ class HomePage extends React.Component {
                     </div>
             )
         }
+        }
     }
-}
 
 export default HomePage;
