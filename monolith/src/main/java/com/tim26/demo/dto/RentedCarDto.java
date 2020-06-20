de596e398cd7596da7abecbd9ce0ee619ff4be33
Mina Maras <minamaras1997@gmail.com>
2020-06-20 18:34:57 +0200

    Create RentedCarDto.java

---
+++ monolith/src/main/java/com/tim26/demo/dto/RentedCarDto.java
@@ -0,0 +1,109 @@
+package com.tim26.demo.dto;
+
+import java.util.List;
+
+public class RentedCarDto {
+    private Long id;
+    private String brand;
+    private String model;
+    private String fuel;
+    private String transmission;
+    private String carClass;
+    private double km;
+    private double kmLimit;
+    private boolean cdw;
+    private int childSeats;
+    private List<byte[]> files;
+
+    public RentedCarDto() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getCarClass() {
+        return carClass;
+    }
+
+    public void setCarClass(String carClass) {
+        this.carClass = carClass;
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public double getKmLimit() {
+        return kmLimit;
+    }
+
+    public void setKmLimit(double kmLimit) {
+        this.kmLimit = kmLimit;
+    }
+
+    public boolean isCdw() {
+        return cdw;
+    }
+
+    public void setCdw(boolean cdw) {
+        this.cdw = cdw;
+    }
+
+    public int getChildSeats() {
+        return childSeats;
+    }
+
+    public void setChildSeats(int childSeats) {
+        this.childSeats = childSeats;
+    }
+
+    public List<byte[]> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<byte[]> files) {
+        this.files = files;
+    }
+}
