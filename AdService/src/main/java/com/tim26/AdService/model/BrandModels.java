88e72e56314863ccea6bf0cee64109be8f85f13a
Mina Maras <minamaras1997@gmail.com>
2020-06-21 13:29:16 +0200

    Update BrandModels.java

--- AdService/src/main/java/com/tim26/AdService/model/BrandModels.java
+++ AdService/src/main/java/com/tim26/AdService/model/BrandModels.java
@@ -1,16 +1,22 @@
 package com.tim26.AdService.model;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "brands")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "BrandModels", namespace = "http://localhost:8084/adservice-schema")
+@XmlRootElement(name = "brandModelsClass")
 public class BrandModels {
 
     @Id
+    @XmlElement
     private String brand;
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @XmlElement
     private List<String> models = new ArrayList<>();
 
     public BrandModels() {
