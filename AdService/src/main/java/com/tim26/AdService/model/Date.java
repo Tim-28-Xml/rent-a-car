e4e91d531bebfbe2d48a34b73f526415ebc591e4
Mina Maras <minamaras1997@gmail.com>
2020-06-21 13:29:27 +0200

    Update Date.java

--- AdService/src/main/java/com/tim26/AdService/model/Date.java
+++ AdService/src/main/java/com/tim26/AdService/model/Date.java
@@ -3,20 +3,27 @@ package com.tim26.AdService.model;
 
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.*;
 import java.time.LocalDate;
 
 
 @Entity
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Date", namespace = "http://localhost:8084/adservice-schema")
+@XmlRootElement(name = "dateClass")
 public class Date {
 
     @Id
+    @XmlElement
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column
+    @XmlElement
     private LocalDate date;
 
     @ManyToOne
+    @XmlElement
     private DateRange dateRange;
 
     public Date() {
