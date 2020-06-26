6e7db6aa74c6371d53d90f04e82d992891dfdeaf
Mina Maras <minamaras1997@gmail.com>
2020-06-26 21:18:53 +0200

    removed comments

--- AdService/src/main/java/com/tim26/AdService/service/PricelistServiceImpl.java
+++ AdService/src/main/java/com/tim26/AdService/service/PricelistServiceImpl.java
@@ -109,28 +109,6 @@ public class PricelistServiceImpl implements PricelistService {
                 return  false;
             }
         }
-
-        /*User user = userService.findByUsername(p.getName());
-        if(user != null) {
-            PriceList priceList = new PriceList();
-            priceList.setCdwPrice(createPricelistDto.getCdwPrice());
-            priceList.setDailyPrice(createPricelistDto.getDailyPrice());
-            priceList.setPricePerExtraKm(createPricelistDto.getPricePerExtraKm());
-            priceList.setName(createPricelistDto.getName());
-            priceList.setUser(user);
-            user.getPriceLists().add(priceList);
-
-            try {
-
-                priceList = pricelistRepository.save(priceList);
-                return true;
-            } catch (Exception e)
-            {
-                LOGGER.error("Failed to save pricelist - Name: {}, \n Daily price: {}, \n Price with cdw: {}, \n Price if the km limit is passed: {} to the database \n", createPricelistDto.getName(), createPricelistDto.getDailyPrice(), createPricelistDto.getCdwPrice(), createPricelistDto.getPricePerExtraKm());
-            }
-        }
-        LOGGER.error("Failed to create the pricelist with name - {} . User with {} username doesn't exist", createPricelistDto.getName(), p.getName());
-        return false;*/
     }
 
     @Override
